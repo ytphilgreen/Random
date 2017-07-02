b64dec0599dd6a2f238e53b68d6479cf5ae0f12d
yolanda philgreen <yolanda.philgreen@gmail.com>
2017-07-02 13:56:35 -0600

    started project for drawing of random numbers like in keno game project

---
+++ src/edu/cnm/deepdive/random/Drawing.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package edu.cnm.deepdive.random;
+
+/**
+ * @author Yolanda Philgreen
+ *
+ */
+public class Drawing {
+
+  /**
+   * @param args
+   */
+  public static void main(String[] args) {
+    // TODO Auto-generated method stub
+
+  }
+
+  private static void random() {
+    
+  }
+  
+}
